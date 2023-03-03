54be9b35f44569280996ab26efd3d4b1b4e78764
Arthur <worldnomad@gmail.com>
2023-03-03 11:46:36 -0800

    update for packaging

--- setup.py
+++ setup.py
@@ -37,9 +37,12 @@ setup(
     keywords="netbox_python",
     name="netbox_python",
     packages=find_packages(include=["netbox_python", "netbox_python.*"]),
+    install_requires=[
+        "requests>=2.20.0,<3.0",
+    ],
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/netbox-community/netbox_python",
-    version="0.1.1",
+    version="0.1.3",
     zip_safe=False,
 )
